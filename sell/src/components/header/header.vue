2e3807709f6631632c60961e2a10eeaaa68439a8
Yqwerty <yuanyangzxc@163.com>
2017-09-19 23:48:23 +0800

    finish little title

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -4,6 +4,19 @@
       <div class="avatar">
         <img :src="seller.avatar" width="64" height="64">
       </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送到
+        </div>
+        <div v-if="seller.supports" class="support">
+          <span class="icon"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
     </div>
     <div class="bulletin-wrapper"></div>
   </div>
@@ -20,6 +33,27 @@ export default {
 </script>
 
 <style lang="stylus" rel='stylesheets/stylus'>
-
+  @import "../../common/stylus/mixin.styl";
+  .header
+    color #fff
+    background #000
+    .content-wrapper
+      padding 24px 12px 18px 24px
+      font-size 0
+      .avatar
+        display inline-block
+      .content 
+        display inline-block
+        font-size 14px
+        margin-left 16px
+        .title
+          margin 2px 0 8px 0
+          .brand
+            display inline-block
+            width 30px
+            height 18px
+            bg-image("brand")
+            background-size 30px 18px
+            background-repeat no-repeat
 </style>
 
