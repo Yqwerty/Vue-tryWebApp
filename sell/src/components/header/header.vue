173660194b24388c09ea2836864c3ee8b52b8d5a
Yqwerty <yuanyangzxc@163.com>
2017-09-20 16:41:36 +0800

    some header components

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -13,8 +13,12 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送到
         </div>
         <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+        <div v-if="seller.supports" class="support-count">
+          <span class="count">{{seller.supports.length}}个</span>
+          <i class="icon-keyboard_arrow_right"></i>
         </div>
       </div>
     </div>
@@ -28,6 +32,9 @@ export default {
     seller: {
       type: Object
     }
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
   }
 };
 </script>
@@ -36,24 +43,76 @@ export default {
   @import "../../common/stylus/mixin.styl";
   .header
     color #fff
-    background #000
+    background #999
     .content-wrapper
+      position relative
       padding 24px 12px 18px 24px
       font-size 0
       .avatar
         display inline-block
+        vertical-align top
+        img 
+          border-radius 2px
       .content 
         display inline-block
-        font-size 14px
         margin-left 16px
-        .title
+        .title  
           margin 2px 0 8px 0
           .brand
             display inline-block
+            vertical-align top
             width 30px
             height 18px
             bg-image("brand")
             background-size 30px 18px
             background-repeat no-repeat
+          .name
+            margin-left 6px
+            font-size 16px
+            line-height 18px
+            font-weight bold
+        .description
+          margin-bottom 10px
+          line-height 12px
+          font-size 12px
+        .support
+          .icon
+            display inline-block
+            vertical-align top
+            width 12px
+            height 12px
+            margin-right 4px
+            background-size 12px
+            background-repeat no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            line-height 12px
+            font-size 10px
+      .support-count
+        position absolute
+        right 12px
+        bottom 14px
+        padding 0 8px
+        height 24px
+        line-height 24px
+        border-radius 14px
+        background-color rgba(0, 0, 0, 0.2)
+        text-align center
+        .count
+          vertical-align top
+          font-size 10px
+        .icon-keyboard_arrow_right
+          margin-left 2px
+          line-height 24px
+          font-size 10px
 </style>
 
