022a7e9dcf827394937626d5176f1dc6510ef85b
Yqwerty <yuanyangzxc@163.com>
2017-09-20 23:16:35 +0800

    finish bulletin

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -22,7 +22,10 @@
         </div>
       </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
   </div>
 </template>
 
@@ -43,7 +46,7 @@ export default {
   @import "../../common/stylus/mixin.styl";
   .header
     color #fff
-    background #999
+    background-color rgba(7, 17, 27, 0.2)
     .content-wrapper
       position relative
       padding 24px 12px 18px 24px
@@ -114,5 +117,32 @@ export default {
           margin-left 2px
           line-height 24px
           font-size 10px
+    .bulletin-wrapper
+      position relative
+      height 28px
+      line-height 28px
+      padding 0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      background-color rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display inline-block
+        vertical-align top
+        margin-top 7px
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+        vertical-align top      
+        margin 0 4px
+        font-size 10px
+      .icon-keyboard_arrow_right
+        position absolute
+        font-size 10px
+        right 12px
+        top 8px
 </style>
 
