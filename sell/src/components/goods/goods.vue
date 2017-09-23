942ac9e8231524fdb6b7924c31ac6e053d32b854
Yqwerty <yuanyangzxc@163.com>
2017-09-23 23:42:07 +0800

    finish header star goods

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -1,13 +1,18 @@
 <template>
- <div>I am goods</div>
+    <div class="goods">
+        <div class="menu-wrapper"></div>
+        <div class="foods-wrapper"></div>
+    </div>
 </template>
 
 <script type="text/ecamscript-6">
-export default { 
+export default {
 
 };
 </script>
 
 <style lang="stylus" rel='stylesheets/stylus'>
-
+    .goods
+        .menu-wrapper
+        .foods-wrapper
 </style> 
