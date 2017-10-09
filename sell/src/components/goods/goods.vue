c86b3b1e2f1603f88683587cf825b46c90289b9c
Yqwerty <yuanyangzxc@163.com>
2017-10-09 23:37:59 +0800

    add better-scroll

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -26,8 +26,8 @@
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span>￥{{food.price}}</span>
-                  <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -148,7 +148,17 @@ export default {
             .extra
               &.count
                 margin-right 12px
-             
+            .price
+              font-weight 700
+              line-height 24px
+              .now
+                margin-right 8px
+                font-size 14px
+                color rgb(240, 20, 20)
+              .old
+                text-decoration line-through
+                font-size 10px
+                color rgb(147, 153 ,159)                
 </style> 
 
 
