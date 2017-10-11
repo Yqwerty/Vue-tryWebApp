5ce89b15435691ccb3124ae7c24ef3b844a7de1d
Yqwerty <yuanyangzxc@163.com>
2017-10-11 20:12:48 +0800

    Oct 11

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="item in goods" class="menu-item" :class="{'current':currentIndex===$index}" @click="selectMenu($index,$event)">
           <span class="text border-1px">
             <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" v-el:foods-wrapper>
       <ul>
-        <li v-for="item in goods" class="foodlist">
+        <li v-for="item in goods" class="foodlist food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -22,8 +22,7 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span class="count">月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
                   <span class="now">￥{{food.price}}</span>
@@ -35,10 +34,14 @@
         </li>
       </ul>
     </div>
+    <shopcart></shopcart>
   </div>
 </template>
 
 <script type="text/ecamscript-6">
+import BScroll from 'better-scroll'
+import shopcart from 'components/shopcart/shopcart'
+
 const ERR_OK = 0
 
 export default {
@@ -49,7 +52,21 @@ export default {
   },
   data() {
     return {
-      goods: []
+      goods: [],
+      listHeight: [],
+      scrollY: 0
+    }
+  },
+  computed: {
+    currentIndex() {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i];
+        let height2 = this.listHeight[i + 1];
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i;
+        }
+      }
+      return 0;
     }
   },
   created() {
@@ -60,10 +77,51 @@ export default {
       response = response.body;
       if (response.errno === ERR_OK) {
         this.goods = response.data;
+        this.$nextTick(() => {
+          this._initScroll();
+          this._calculateHeight();
+        })
       }
     })
   },
+  methods: {
+    selectMenu(index, event) {
+      if (!event._constructed) {
+        return
+      }
+      let foodlist = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+      let el = foodlist[index];
+      this.foodsScroll.scrollToElement(el, 300)
+      console.log(index)
+    },
+    _initScroll() {
+      this.menuScroll = new BScroll(this.$els.menuWrapper, {
+        click: true
+      });
 
+      this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+        probeType: 3
+      });
+
+      this.foodsScroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y));
+      });
+
+    },
+    _calculateHeight() {
+      let foodlist = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < foodlist.length; i++) {
+        let item = foodlist[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
+    }
+  },
+  components: {
+    shopcart
+  }
 };
 </script>
 
@@ -85,7 +143,15 @@ export default {
         height 54px
         width 56px
         padding 0 12px
-        line-height 14px  
+        line-height 14px 
+        &.current
+          position relative
+          z-index 10
+          margin-top -1px
+          background #fff
+          font-weight 700
+          .text 
+            border-none()
         .icon
           display inline-block
           vertical-align top
@@ -145,8 +211,9 @@ export default {
               color rgb(147, 153 ,159)
             .desc
               margin-bottom 8px
+              line-height 12px
             .extra
-              &.count
+              .count
                 margin-right 12px
             .price
               font-weight 700
