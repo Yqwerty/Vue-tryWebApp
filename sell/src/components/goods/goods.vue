fa0721fe9aa7002e29972ee293f30fa45f010e4e
Yqwerty <yuanyangzxc@163.com>
2017-10-12 10:26:57 +0800

    Oct 12

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -34,7 +34,7 @@
         </li>
       </ul>
     </div>
-    <shopcart></shopcart>
+    <shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
 </template>
 
--- sell/src/components/shopcart/shopcart.vue
+++ sell/src/components/shopcart/shopcart.vue
@@ -1,23 +1,42 @@
 <template>
-	<div class="shopcart">
-		<div class="content">
-			<div class="content-left">
-				<div class="logo-wrapper">
-					<div class="logo">
-						<i class="icon-shopping_cart"></i>
-					</div>
+  <div class="shopcart">
+    <div class="content">
+      <div class="content-left">
+        <div class="logo-wrapper">
+          <div class="logo">
+            <i class="icon-shopping_cart"></i>
+          </div>
+        </div>
+        <div class="price">0元</div>
+        <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
+      </div>
+      <div class="content-right">
+				<div class="pay">
+					￥{{minPrice}}元起送
 				</div>
-				<div class="price">0元</div>
-				<div class="desc"></div>
 			</div>
-			<div class="content-right"></div>
-		</div>
-	</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    selectFoods: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    deliveryPrice: {
+      type: Number,
+      default: 0
+    },
+    minPrice: {
+      type: Number,
+      default: 0
+    }
+  }
 };
 </script>
 
@@ -34,6 +53,7 @@ export default {
 			display flex
 			background #141d27
 			font-size 0
+			color rgba(255, 255 ,255, 0.4)								
 			.content-left
 				flex 1
 				.logo-wrapper
@@ -68,11 +88,20 @@ export default {
 					border-right 1px solid rgba(255, 255 ,255, 0.1)
 					font-size 16px
 					font-weight 700
-					color rgba(255, 255 ,255, 0.4)
 				.desc
 					display inline-block
-				
+					vertical-align top
+					margin 12px 0 0 12px
+					line-height 24px
+					font-size 10px
 			.content-right
 				flex 0 0 105px
 				width 105px
+				.pay
+					height 48px
+					line-height 48px
+					text-align center
+					font-size 12px
+					font-weight 700
+					background #2b333b
 </style>
