c082098c80477b7f73240c2be3ca9d01cb325764
Yqwerty <yuanyangzxc@163.com>
2017-10-12 16:13:28 +0800

    add cartcontrol template

---
+++ sell/src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="cartcontril"></div>
+</template>
+
+<script type="text/ecamscript-6">
+export default {
+
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">   
+
+</style>
