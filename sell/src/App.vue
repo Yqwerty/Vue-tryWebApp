07120cd1b84024d49b40bdb3ff0e01924d8416d3
Yqwerty <yuanyangzxc@163.com>
2017-11-28 09:27:44 +0800

    finished

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -12,26 +12,31 @@
         <a v-link="{path:'/seller'}">商家</a>
       </div>
     </div>
-    <router-view :seller="seller"></router-view>
+    <router-view :seller="seller" keep-alive></router-view>
   </div>
 </template>
 
 <script>
 import header from './components/header/header';
-
+import { urlParse } from 'common/js/util';
 const ERR_OK = 0;
 
 export default {
   data() {
     return {
-      seller: {}
+      seller: {
+        id: (() => {
+          let queryParam = urlParse();
+          return queryParam.id;
+        })()
+      }
     };
   },
   created() {
-    this.$http.get('/api/seller').then((response) => {
+    this.$http.get('/api/seller?id=' + this.seller.id).then(response => {
       response = response.body;
       if (response.errno === ERR_OK) {
-        this.seller = response.data;
+        this.seller = Object.assign({}, this.seller, response.data);
       }
     });
   },
